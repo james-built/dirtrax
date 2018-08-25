ef4266b2dca0c4deeeed12a8614f58c77b771b82
james-built <jgbuilt@gmail.com>
2018-08-25 15:48:00 +1200

    added get route to send map template

--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
 const app = require('express')
-
+const path = require('path')
 const router = app.Router()
 
 module.exports = router
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
